c4776344e1a4985b5a790bde6bf671e20c6b489b
Jose Soto <jsoto7087@gmail.com>
2021-06-13 17:06:56 -0400

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+
+            function writeCards(names, stringOfNames){
+                const names2 = [];
+                
+                for (let i = 0; i < names.length; i++) {
+                    let namesReturn = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+                    names2.push(namesReturn);
+                  
+
+                }
+
+
+                return names2;
+
+            }
+
+
+            function countDown(numberCountDown) {     
+        
+                // let i = numberCountDown;
+
+                
+                    while ( numberCountDown >= 0) {
+                        console.log(numberCountDown);
+                        numberCountDown -= 1
+                        
+            } 
+        }
