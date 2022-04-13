8e4fdbd40aa00a5838ad2f8938300c7ac59acfc8
Jose Soto <jsoto7087@gmail.com>
2022-04-12 20:02:29 -0400

    commit tree 856994edf81893e78444c7d93af99a0f48aa653e

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
+function writeCards(names, stringOfNames) {
+    const names2 = [];
 
+    for (let i = 0; i < names.length; i++) {
+        let namesReturn = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        names2.push(namesReturn);
 
-            function writeCards(names, stringOfNames){
-                const names2 = [];
-                
-                for (let i = 0; i < names.length; i++) {
-                    let namesReturn = `Thank you, ${names[i]}, for the wonderful surprise gift!`
-                    names2.push(namesReturn);
-                  
+    }
+    return names2;
+}
 
-                }
+function countDown(numberCountDown) {
 
-
-                return names2;
-
-            }
-
-
-            function countDown(numberCountDown) {     
-        
-                // let i = numberCountDown;
-
-                
-                    while ( numberCountDown >= 0) {
-                        console.log(numberCountDown);
-                        numberCountDown -= 1
-                        
-            } 
-        }
+    while (numberCountDown >= 0) {
+        console.log(numberCountDown);
+        numberCountDown -= 1
+    }
+}
